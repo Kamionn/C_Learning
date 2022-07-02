1e648ae9961d1b7faa31eeb71afffae59e093473
Kamionn <mattheo.lefur41@gmail.com>
2022-07-02 15:04:19 +0200

    learn

--- Chapitre2/chap2.c
+++ Chapitre2/chap2.c
@@ -6,6 +6,12 @@
 int main(int argc, char *argv[]) // Équivalent de int main()
 {
   int kamion; 
-     
+  kamion = 1; 
+  printf("%d chocolat", kamion);
+
+  int kamvalue;
+  const int kamvalue = 5; // Valeur constante 
+   printf("%d vélo", kamvalue);
+
   return 0;
 }
