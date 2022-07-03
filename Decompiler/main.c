1c7a009cb5a0810689f51527bb107b177c8cb6ef
Kamionn <mattheo.lefur41@gmail.com>
2022-07-03 10:34:07 +0200

    add_decompil

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[]) // Équivalent de int main()
 {
-    printf("Hello world!\n");
-    return 0;
+  // instruction
+  int kamion;
+  kamion = 1;
+  printf("%d chocolat", kamion);
+
+  const int kamvalue = 5; // Valeur constante
+  printf("%d vélo", kamvalue);
+
+  return 0;
 }
