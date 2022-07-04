1f04f520b6078bdcb44bccc97a63a42fd0ac724a
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 15:28:32 +0200

    Create exercice_Chap.3.3.c

---
+++ Chapitre3/exercice_Chap.3.3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Calcul de fonction + Incrémentation 
+
+int main()
+{
+    int nombre1 = 1, nombre2 = 0, resultat;
+
+    nombre1 = nombre1 + 0.02;
+    resultat = nombre1 + nombre2;
+
+    printf("%d",resultat);
+    
+}
