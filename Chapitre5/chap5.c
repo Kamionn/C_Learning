1c2cb1cdf59105199e32522fc024bfbcbd1f369b
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 19:25:33 +0200

    Update chap5.c

--- Chapitre5/chap5.c
+++ Chapitre5/chap5.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+int condition = 0;
+
+int main()
+{
+while (condition != 1)
+{
+    printf("Tapez le nombre 1 ! ");
+    scanf("%d", &condition);
+}
+}
