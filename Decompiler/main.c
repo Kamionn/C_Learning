ebdcdd01f52bb8df527283f94a790ecd14a28884
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 18:36:13 +0200

    up

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <math.h>
 
-
-int variable()
+int main()
 {
-    double absolu = 0, nombre = -27;
-    absolu = fabs(nombre);
-    printf("%d", nombre);
+    int age = 2;
+
+     if (age >= 18) // Si l'âge est supérieur ou égal à 18
+    {
+        printf ("Vous etes majeur !");
+    }
+        else if ( age > 4 ) // Sinon, si l'âge est au moins supérieur à 4
+    {
+        printf ("Bon t'es pas trop jeune quand meme...");
+    }
+        else // Sinon...
+    {
+        printf ("Aga gaa aga gaaa"); // Langage bébé, vous pouvez pas comprendre
+}
 }
