dc3ceefebc415057f2007895ae90e56f9db9c82d
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 16:18:05 +0200

    up

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -1,26 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main()
+
+int variable()
 {
-    int nombre1 = 0, nombre2 = 0, addition = 0, soustraction = 0, multiplication = 0, division = 0, modulos = 0;
-    printf("Choisissez un nombre : ");
-    scanf("%d", &nombre1);
-    printf("Choisissez un nombre : ");
-    scanf("%d", &nombre2);
-
-    addition = nombre1 + nombre2;
-    printf("Addition : %d \n", addition);
-
-    soustraction = nombre1 - nombre2;
-    printf("Soustraction : %d \n", soustraction);
-
-    multiplication = nombre1 * nombre2;
-    printf("Multiplication : %d \n", multiplication);
-
-    division = nombre1 / nombre2;
-    printf("Division : %d \n", division);
-
-    modulos = nombre1 % nombre2;
-    printf("Division : %d \n", modulos);
+    double absolu = 0, nombre = -27;
+    absolu = fabs(nombre);
+    printf("%d", nombre);
 }
