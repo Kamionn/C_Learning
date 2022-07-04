57d2d4fe951c47c47c1a77542ef5e8ffca1bbcd1
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 15:19:18 +0200

    Update exercice_Chap3.1.c

--- Chapitre3/exercice_Chap3.1.c
+++ Chapitre3/exercice_Chap3.1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Calcul Basique
+
 int main()
 {
-    int addition = 0;
+    int addition = 0, soustraction = 0, multiplication = 0, division = 0;
+    
     addition = 18 + 320;
     printf("Le resultat du calcule : %d \n", addition);
 
-    int soustraction = 0;
     soustraction = 18 - 320;
     printf("Le resultat du calcule : %d \n", soustraction);
 
-    int multiplication = 0;
     multiplication = 18 * 320;
     printf("Le resultat du calcule : %d \n", multiplication);
 
-    int division = 0;
     division = 18 / 320;
     printf("Le resultat du calcule : %d \n", division);
 }
