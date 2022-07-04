f273ea2338c11f7c4450f00e61b43e5a6425dc92
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 15:19:43 +0200

    up

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -3,12 +3,24 @@
 
 int main()
 {
-    int nombre1 = 0;
+    int nombre1 = 0, nombre2 = 0, addition = 0, soustraction = 0, multiplication = 0, division = 0, modulos = 0;
     printf("Choisissez un nombre : ");
     scanf("%d", &nombre1);
-
-    int nombre2 = 0;
     printf("Choisissez un nombre : ");
     scanf("%d", &nombre2);
 
+    addition = nombre1 + nombre2;
+    printf("Addition : %d \n", addition);
+
+    soustraction = nombre1 - nombre2;
+    printf("Soustraction : %d \n", soustraction);
+
+    multiplication = nombre1 * nombre2;
+    printf("Multiplication : %d \n", multiplication);
+
+    division = nombre1 / nombre2;
+    printf("Division : %d \n", division);
+
+    modulos = nombre1 % nombre2;
+    printf("Division : %d \n", modulos);
 }
