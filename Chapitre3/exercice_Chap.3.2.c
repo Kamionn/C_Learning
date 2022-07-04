e44083cac8b1415d98c011390a7692947367ea0a
Kamionn <mattheo.lefur41@gmail.com>
2022-07-04 15:10:08 +0200

    Update exercice_Chap.3.2.c

--- Chapitre3/exercice_Chap.3.2.c
+++ Chapitre3/exercice_Chap.3.2.c
@@ -5,23 +5,23 @@ int main()
 {
     int nombre1 = 0; 
     printf("Choisissez un nombre : ");
-    scanf("%d", &nombre1);
+    scanf("Nombre2 choisis : %d /n", &nombre1);
 
     int nombre2 = 0; 
     printf("Choisissez un nombre : ");
-    scanf("%d", &nombre2);
+    scanf("Nombre2 choisis : %d /n", &nombre2);
 
     int addition = 0, soustraction = 0, multiplication = 0, division = 0;
     addition = nombre1 + nombre2;
-    printf("Addition : %d", addition);
+    printf("Addition : %d /n", addition);
 
     soustraction = nombre1 - nombre2;
-    printf("Soustraction : %d", soustraction);
+    printf("Soustraction : %d /n", soustraction);
 
     multiplication = nombre1 * nombre2;
-    printf("Multiplication : %d", multiplication);
+    printf("Multiplication : %d /n", multiplication);
 
     division = nombre1 / nombre2;
-    printf("Division : %d", division);
+    printf("Division : %d /n", division);
     
 }
