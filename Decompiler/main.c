3e6ed9ae8e7f82109c111ee4ea4fd9a3c0bb73a0
Kamionn <mattheo.lefur41@gmail.com>
2022-07-05 17:59:57 +0200

    up

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -3,8 +3,18 @@
 
 int triple(int nombre)
 {
-    int resultat = 0;
+    return 3 * nombre;
+}
 
-    resultat = 3 * nombre;  // On multiplie le nombre fourni par 3
-    return resultat;       // On retourne la variable resultat qui vaut le triple de nombre
+int main(int argc, char *argv[])
+{
+    int nombreEntre = 0, nombreTriple = 0;
+
+    printf("Entrez un nombre... ");
+    scanf("%d", &nombreEntre);
+
+    nombreTriple = triple(nombreEntre);
+    printf("Le triple de ce nombre est %d\n", nombreTriple);
+
+    return 0;
 }
