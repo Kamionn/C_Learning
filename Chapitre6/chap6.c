d1862b6886e583e15b3cd1b8243f9c8064729d10
Kamionn <mattheo.lefur41@gmail.com>
2022-07-05 18:27:49 +0200

    Update chap6.c

--- Chapitre6/chap6.c
+++ Chapitre6/chap6.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int triple(int nombre)
+// Fonction secondaire
+int triple(int nombre) // 6
 {
-    return 3 + nombre;
+    return 3 * nombre; // 7
 }    
 
-int main(int argc, char *argv[])
+// Fonction primaire
+int main(int argc, char *argv[]) // 1
 {
-    int nombreEntre = 0, nombreTriple = 0;
+    int nombreEntre = 0, nombreTriple = 0; // 2
     
-    printf("Entrez un nombre... ");
-    scanf("%d", &nombreEntre);
+    printf("Entrez un nombre... "); // 3
+    scanf("%d", &nombreEntre); // 4
     
-    nombreTriple = triple(nombreEntre);
-    printf("Le triple de ce nombre est %d\n", nombreTriple);
+    nombreTriple = triple(nombreEntre); // 5
+    printf("Le triple de ce nombre est %d\n", nombreTriple); // 8
     
-    return 0;
+    return 0; // 9
 }
