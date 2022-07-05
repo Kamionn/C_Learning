beea30a30555fa3808aa888d1ecf4e2696f5e093
Kamionn <mattheo.lefur41@gmail.com>
2022-07-05 14:27:32 +0200

    Create exercice_Chap5.c

---
+++ Chapitre5/exercice_Chap5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+    int boucle = 10;
+
+    while (boucle != 0)
+    {
+         printf("Nombre de boucle : %d\n", boucle);
+         boucle --; // Incrémentation
+         boucle ++; // Infinis
+    }
+}
