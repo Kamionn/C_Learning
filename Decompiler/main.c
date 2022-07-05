84cb29420d4964b535ce3b3502d76929f18912cb
Kamionn <mattheo.lefur41@gmail.com>
2022-07-05 14:27:36 +0200

    up

--- Decompiler/main.c
+++ Decompiler/main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 int main()
 {
-    int condition = 50;
+    int boucle = 10;
 
-    while (condition != 0)
+    while (boucle != 0)
     {
-        printf("%d\n", condition);
-        condition--;
+         printf("Nombre de boucle : %d\n", boucle);
+         boucle --; // Incrémentation
+        // boucle ++; // Infinis
     }
-    printf("%d\n", condition);
-    return 0;
 }
