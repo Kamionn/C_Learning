3737a6eb373a60cb41c0b3a69f54c2966695597b
Kamionn <mattheo.lefur41@gmail.com>
2022-07-06 09:14:19 +0200

    push

--- Chapitre7/Header/air.c
+++ Chapitre7/Header/air.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+#include "air.h"
+
+double prototype(double largeur, double hauteur)
+{
+    return largeur * hauteur;
+}
--- Chapitre7/Header/main.c
+++ Chapitre7/Header/main.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "air.h"
+
+int main()
+{
+    double resultat;
+    resultat = airRectangle(10.0, 20.0);
+
+    printf("%d", resultat);
+    return 0;
+}
