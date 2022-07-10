cb3f0931630b67df0b647227f2aef46cc817c1c4
Kamionn <mattheo.lefur41@gmail.com>
2022-07-10 11:47:52 +0200

    push

---
+++ Chapitre9/exercice_Chap9.c
@@ -0,0 +1,21 @@
+//
+// Created by Kamion on 10/07/2022.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void MultiplicationFruit(int *FruitMultiplier);
+
+int main()
+{
+    int fruit = 15;
+    MultiplicationFruit(&fruit);
+    printf("Vous avez %d fruits", fruit);
+    return 0;
+}
+
+void MultiplicationFruit(int *FruitMultiplier)
+{
+    *FruitMultiplier *= 3; // On multiplie par 3 la valeur de nombre
+}
