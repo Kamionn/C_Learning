59090987ff41e2aeb9d01eb8b7998aafc916643b
Kamionn <mattheo.lefur41@gmail.com>
2022-07-11 10:05:52 +0200

    push

---
+++ Chapitre9/exercice_Chap9.1.c
@@ -0,0 +1,18 @@
+//
+// Created by Kamion on 10/07/2022.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int a = 10;
+    int * pa = &a;
+
+    printf('a = %d (adresse : %p)', a, &a);
+
+}
