4fc32061b86bb11de89bcc990eca27eb18da59a6
Kamionn <mattheo.lefur41@gmail.com>
2022-07-14 11:24:51 +0200

    push

--- Chapitre9/exercice_Chap9.1.c
+++ Chapitre9/exercice_Chap9.1.c
@@ -1,18 +1,24 @@
 //
 // Created by Kamion on 10/07/2022.
 //
-
+// Les pointeurs
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int main()
+void variable(nombreA, nombreB)
 {
-    int a = 10;
-    int * pa = &a;
 
-    printf('a = %d (adresse : %p)', a, &a);
+}
+
+
+int main(void)
+{
+    int nombreA = 10;
+    int nombreB = 20;
+
+    printf('Nombre A %d\n Nombre B %d', nombreA, nombreB);
+
+    variable = (nombreA, nombreB);
 
 }
---
+++ Exercice/formate.c
@@ -0,0 +1,22 @@
+//
+// Created by Kamion on 14/07/2022.
+//
+// Exercice C #1
+// [Révision : Affichage / saisie, Variables]
+// > Ecrire un programme qui demande un nombre entier à l'utilisateur
+// Et affiche la valeur de la saisie en octal et héxadécimal
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    printf("Entrez un nombre entier :\n");
+    scanf("%d", &a);
+    printf("Valeur en octal : %o\n", a);
+    printf("Valeur en hexa : %x\n", a);
+    return 0;
+}
+
